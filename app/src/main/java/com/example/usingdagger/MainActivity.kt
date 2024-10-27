71f72fbf1b73a8cf7f3c0704262104f652f28437
ebruaeo <ebruaeo@hotmail.com>
2024-10-28 00:03:29 +0300

    using Dagger to inject dependencies into the MainActivity.

---
+++ app/src/main/java/com/example/usingdagger/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.usingdagger
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import javax.inject.Inject
+
+class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var userRepository: UserRepository
+
+    @Inject
+    lateinit var welcomeMessage: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        (application as MyApplication).appComponent.inject(this)
+
+        println("User name: ${userRepository.getUserName()}")
+        println("Message: $welcomeMessage")
+    }
+
+}
